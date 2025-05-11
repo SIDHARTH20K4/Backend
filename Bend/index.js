81c4af35651ebdcb30006afcab225a03d0356a64
SIDHARTH20K4 <91970588+SIDHARTH20K4@users.noreply.github.com>
2025-05-11 23:15:07 +0530

    changes

--- Bend/index.js
+++ Bend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const product = require("./model/product.model.js");
+const Product = require('./model/product.model.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +19,35 @@ app.get('/', (req, res) => {
     res.send("Hello from node API");
 });
 
+app.get('/api/products', async(req, res) => {
+    try {
+        const products = await product.find({});
+        res.status(500).json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+app.get('/api/products/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const Product = await product.findById(id);
+        res.status(200).json(Product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+app.put('/api/products/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const Product = await product.findByIdAndUpdate(id, req.body);
+        if (!Product) {
+            return res.status(404).json({ message: "product not found" });
+        }
+
+    }
+})
 
 app.post("/api/products", async(req, res) => {
     try {
